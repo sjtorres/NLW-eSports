4788b9c99367484d08215179cb021654c6aa8958
Sandro Torres <sandrotorres.ti@gmail.com>
2022-09-21 10:42:39 -0300

    Criado componentes AdBanner e GameBanner

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,54 +1,51 @@
+import { useState, useEffect } from 'react';
+
 import './styles/main.css';
 
 import logoImg from './assets/logo-nlw-esports.svg';
 import { GameBanner } from './components/GameBanner';
 import { CreateAdBanner } from './components/CreateAdBanner';
 
+interface Game {
+    id: string;
+    title: string;
+    bannerUrl: string;
+    _count: {
+        ads: number;
+    }
+}
+
 function App() {
+    const [games, setGames] = useState<Game[]>([])
+
+    useEffect(() => {
+        fetch('http://localhost:3333/games')
+        .then(response => response.json())
+        .then(data => {
+            setGames(data)
+        })
+    }, [])
+
  return (
-  <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
-    <img src={logoImg} alt='' />
-    <h1 className='text-6xl text-white font-black mt-20'>
+  <div className='max-w-[1000px] mx-auto flex flex-col items-center my-20'>
+    <img src={logoImg} alt="" />
+    
+    <h1 className='text-4xl text-white font-black mt-20'>
         Seu <span className='text-transparent bg-nlw-gradient bg-clip-text'>duo</span> está aqui.
     </h1>
 
-    <div className='grid grid-cols-6 gap-6 mt-16'>
-        <GameBanner bannerUrl="/game-1.png" title="League of Legends" adsCount={5}/>
-        <a href="" className='relative rounded-lg overflow-hidden'>
-            <img src="/game-2.png" alt="" />
-            <div className='w-ful pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-                <strong className='font-bold text-white block'>Dota 2</strong>
-                <span className='text-zinc-300 text-sm block'>4 anúncios</span>
-            </div>
-        </a>
-        <a href="" className='relative rounded-lg overflow-hidden'>
-            <img src="/game-3.png" alt="" />
-            <div className='w-ful pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-                <strong className='font-bold text-white block'>Counter Strike</strong>
-                <span className='text-zinc-300 text-sm block'>4 anúncios</span>
-            </div>
-        </a>
-        <a href="" className='relative rounded-lg overflow-hidden'>
-            <img src="/game-4.png" alt="" />
-            <div className='w-ful pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-                <strong className='font-bold text-white block'>Apex Legends</strong>
-                <span className='text-zinc-300 text-sm block'>4 anúncios</span>
-            </div>
-        </a>
-        <a href="" className='relative rounded-lg overflow-hidden'>
-            <img src="/game-5.png" alt="" />
-            <div className='w-ful pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-                <strong className='font-bold text-white block'>Fortnite</strong>
-                <span className='text-zinc-300 text-sm block'>4 anúncios</span>
-            </div>
-        </a>
-        <a href="" className='relative rounded-lg overflow-hidden'>
-            <img src="/game-6.png" alt="" />
-            <div className='w-ful pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-                <strong className='font-bold text-white block'>World of Warcraft</strong>
-                <span className='text-zinc-300 text-sm block'>4 anúncios</span>
-            </div>
-        </a>
+    <div className="grid grid-cols-6 gap-6 mt-16">
+        {games.map(game => {
+            return (
+                <GameBanner
+                    key={game.id} 
+                    title={game.title} 
+                    bannerUrl={game.bannerUrl} 
+                    adsCount={game._count.ads}
+                />
+            )
+        })}
+        
     </div>
 
     <CreateAdBanner />
--- web/src/components/CreateAdBanner.tsx
+++ web/src/components/CreateAdBanner.tsx
@@ -2,15 +2,15 @@ import { MagnifyingGlassPlus } from 'phosphor-react';
 
 export function CreateAdBanner() {
     return (
-        <div className='pt-1 bg-nlw-gradient mt-8 self-stretch rounded-l overflow-hidden'>
-        <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center'>
+        <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8 overflow-hidden">
+        <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
             <div>
-                <strong className='text-2xl font-black text-white block'>Não encontrou o seu duo?</strong>
-                <span className='text-zinc-400 block'>Publique um anúncio para encontrar novos players!</span>
+                <strong className="text-2xl font-black text-white block">Não encontrou o seu duo?</strong>
+                <span className="text-zinc-400 block">Publique um anúncio para encontrar novos players!</span>
             </div>
 
-            <button className='py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3'>
-                <MagnifyingGlassPlus size={24}/>
+            <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
+                <MagnifyingGlassPlus size={24} />
                 Publicar anúncio
             </button>
         </div>
